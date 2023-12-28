1e1b275fb7761f5e75733e7cc3adb816603849cf
alexltyt <alexltytca@gmail.com>
2023-12-28 15:56:32 -0700

    Update App.js styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import UserList from './components/UserList';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-gradient-to-br from-orange-300  via-orange-200 to-orange-300 w-screen md:w-2/3 mx-auto md:rounded-3xl h-screen text-gray-800 ">
       <UserList />
     </div>
   );
